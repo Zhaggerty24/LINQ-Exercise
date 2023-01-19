9eb8c80b3543f4442287b0a9607b7b4e1c09bf24
Zachary Haggerty <zhaggerty24@gmail.com>
2023-01-19 12:10:28 -0500

    initial commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGames = new List<string>() {"Forza", "Apex","COD", "Madden" };
+
+            videoGames.OrderBy(x => x.Length).ToList().ForEach(Console.WriteLine);
+           
+            
         }
     }
 }
